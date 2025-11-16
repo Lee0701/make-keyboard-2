4552836c686b6dd53dbe70e108a576779bb0e1a7
Lee0701 <hgl1002@naver.com>
2025-11-17 02:19:41 +0900

    Temporarily add uppercase layout

--- app/src/main/java/ee/oyatl/ime/make2/IMEService.kt
+++ app/src/main/java/ee/oyatl/ime/make2/IMEService.kt
@@ -7,50 +7,37 @@ import android.view.View
 
 class IMEService: InputMethodService() {
     lateinit var keyboardView: KeyboardView
+    lateinit var keyboards: List<Keyboard>
 
-    private val template: KeyboardTemplate = KeyboardTemplate(listOf(
-        KeyboardTemplate.Row(listOf(
-            KeyboardTemplate.Key('q'.code),
-            KeyboardTemplate.Key('w'.code),
-            KeyboardTemplate.Key('e'.code),
-            KeyboardTemplate.Key('r'.code),
-            KeyboardTemplate.Key('t'.code),
-            KeyboardTemplate.Key('y'.code),
-            KeyboardTemplate.Key('u'.code),
-            KeyboardTemplate.Key('i'.code),
-            KeyboardTemplate.Key('o'.code),
-            KeyboardTemplate.Key('p'.code)
-        )),
-        KeyboardTemplate.Row(listOf(
-            KeyboardTemplate.Spacer(0.5f),
-            KeyboardTemplate.Key('a'.code),
-            KeyboardTemplate.Key('s'.code),
-            KeyboardTemplate.Key('d'.code),
-            KeyboardTemplate.Key('f'.code),
-            KeyboardTemplate.Key('g'.code),
-            KeyboardTemplate.Key('h'.code),
-            KeyboardTemplate.Key('j'.code),
-            KeyboardTemplate.Key('k'.code),
-            KeyboardTemplate.Key('l'.code),
-            KeyboardTemplate.Spacer(0.5f)
-        )),
-        KeyboardTemplate.Row(listOf(
-            KeyboardTemplate.Key(keyCode = KeyEvent.KEYCODE_SHIFT_LEFT, width = 1.5f),
-            KeyboardTemplate.Key('z'.code),
-            KeyboardTemplate.Key('x'.code),
-            KeyboardTemplate.Key('c'.code),
-            KeyboardTemplate.Key('v'.code),
-            KeyboardTemplate.Key('b'.code),
-            KeyboardTemplate.Key('n'.code),
-            KeyboardTemplate.Key('m'.code),
-            KeyboardTemplate.Key(keyCode = KeyEvent.KEYCODE_DEL, width = 1.5f)
-        )),
-        KeyboardTemplate.Row(listOf(
-            KeyboardTemplate.Spacer(3f),
-            KeyboardTemplate.Key(keyCode = KeyEvent.KEYCODE_SPACE, width = 4f),
-            KeyboardTemplate.Spacer(3f)
+    private val layout = listOf(
+        "qwertyuiop",
+        "asdfghjkl",
+        "zxcvbnm"
+    )
+
+    private fun buildTemplate(layout: List<String>): KeyboardTemplate {
+        val rows = layout.map { row -> row.map { KeyboardTemplate.Key(it.code) }.toTypedArray() }
+        return KeyboardTemplate(listOf(
+            KeyboardTemplate.Row(listOf(
+                *rows[0]
+            )),
+            KeyboardTemplate.Row(listOf(
+                KeyboardTemplate.Spacer(0.5f),
+                *rows[1],
+                KeyboardTemplate.Spacer(0.5f)
+            )),
+            KeyboardTemplate.Row(listOf(
+                KeyboardTemplate.Key(keyCode = KeyEvent.KEYCODE_SHIFT_LEFT, width = 1.5f),
+                *rows[2],
+                KeyboardTemplate.Key(keyCode = KeyEvent.KEYCODE_DEL, width = 1.5f)
+            )),
+            KeyboardTemplate.Row(listOf(
+                KeyboardTemplate.Spacer(3f),
+                KeyboardTemplate.Key(keyCode = KeyEvent.KEYCODE_SPACE, width = 4f),
+                KeyboardTemplate.Spacer(3f)
+            ))
         ))
-    ))
+    }
 
     private val keyboardTheme: DefaultKeyboardStyle.Theme = DefaultKeyboardStyle.Theme(
         keyboardBackground = Color.valueOf(0xffe8e8e8.toInt()),
@@ -96,12 +83,16 @@ class IMEService: InputMethodService() {
             keyCode: Int,
             state: ModifierKeyHandler.ModifierState
         ) {
-            if(state == ModifierKeyHandler.ModifierState.Released) {
-                keyboardView.style = DefaultKeyboardStyle(keyboardTheme)
-            } else if(state == ModifierKeyHandler.ModifierState.Pressed) {
-                keyboardView.style = DefaultKeyboardStyle(keyboardTheme.copy(keyboardBackground = Color.valueOf(Color.DKGRAY)))
-            } else {
-                keyboardView.style = DefaultKeyboardStyle(keyboardTheme.copy(keyboardBackground = Color.valueOf(Color.BLACK)))
+            when (state) {
+                ModifierKeyHandler.ModifierState.Released -> {
+                    keyboardView.keyboard = keyboards[0]
+                }
+                ModifierKeyHandler.ModifierState.Pressed -> {
+                    keyboardView.keyboard = keyboards[1]
+                }
+                ModifierKeyHandler.ModifierState.Locked -> {
+                    keyboardView.keyboard = keyboards[1]
+                }
             }
         }
     }
@@ -118,8 +109,13 @@ class IMEService: InputMethodService() {
             width = resources.displayMetrics.widthPixels,
             height = 700
         )
+        val keyboards = listOf(
+            Keyboard(buildTemplate(layout), params),
+            Keyboard(buildTemplate(layout.map { it.uppercase() }), params)
+        )
+        this.keyboards = keyboards
         keyboardView = KeyboardView(this, null)
-        keyboardView.keyboard = Keyboard(template, params)
+        keyboardView.keyboard = keyboards[0]
         keyboardView.style = DefaultKeyboardStyle(keyboardTheme)
         keyboardView.listener = keyboardListener
     }
